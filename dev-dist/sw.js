4109e38ba33841407fe834bba9b67d142dbe558f
Bhavya Madan <bindubhdh@gmail.com>
2026-01-07 16:48:35 +0530

    WIP: experimental UI changes (not production-ready)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gsd9ak2apu8"
+    "revision": "0.4in2nn0gri"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
